8daa1f4dd7c84dc018057f62c3ed48bad39a8623
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-07-31 23:40:36 -0500

    feat: Agregando repository de pedido

--- infrastructure/database/database.ts
+++ infrastructure/database/database.ts
@@ -11,19 +11,8 @@ export async function inicializarBaseDatos(db: SQLiteDatabase) {
      id INTEGER PRIMARY KEY AUTOINCREMENT,
      fecha DATE NOT NULL,
      total DECIMAL NOT NULL,
-     estado TEXT NOT NULL DEFAULT 'PENDIENTE'
-    );
-    `
-    );
-    console.log('[DB INIT] Creando tabla detalle_pedido si no exite');
-    await db.execAsync(`
-    PRAGMA journal_mode = WAL;
-    
-    Create TABLE IF NOT EXISTS detalle_pedido(
-     id INTEGER PRIMARY KEY AUTOINCREMENT,
-     pedido_id INT NOT NULL,
-     funko_nombre TEXT NOT NULL,
-     precio DECIMAL NOT NULL
+     estado TEXT NOT NULL DEFAULT 'PENDIENTE',
+     cantidad_productos INT NOT NULL
     );
     `
     );
